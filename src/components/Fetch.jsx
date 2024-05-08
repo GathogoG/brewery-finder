7c2c88b00c3b6d61d025798554d173ca2cc9e109
CharlesKibet <charlesbiegon973@gmail.com>
2024-05-08 13:59:19 +0300

    Db.json setup and fetching

---
+++ src/components/Fetch.jsx
@@ -0,0 +1,30 @@
+//Fetching data here from the the db.json
+
+import React, { useEffect, useState } from 'react'
+
+function Fetch() {
+  const [breweries,setBreweries] = useState([]);
+  //function to fetch data
+  function fetchData(){
+    return fetch('http://localhost:3000/breweries',{
+      method :'GET',
+      headers:{
+        'Content-Type':'application/json',
+      },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+      setBreweries(data);
+    })
+  }
+  useEffect(() =>{
+    fetchData();
+  },[]);
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Fetch;
