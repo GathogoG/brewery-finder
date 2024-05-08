128d9063e836311ec1819f0f3142ddfa4151c8e5
Rizz-ke <wafiqmohameddoo@gmail.com>
2024-05-08 06:13:26 +0300

    Search bar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+// Import necessary details
 import React from 'react';
 import { Container } from '@chakra-ui/react';
 import SearchBar from './components/SearchBar';
 
+// Function component for App
 function App() {
+  // Return  with Container component and SearchBar component
   return (
-    <Container maxW="container.md" centerContent>
-      <SearchBar />
+    <Container maxW="container.md" centerContent> {/* Container component */}
+      <SearchBar /> {/* SearchBar component */}
     </Container>
   );
 }
+
 
 export default App;
