909425b6a9121f1c2c54b7c23004fbc8a947b6e3
Rizz-ke <wafiqmohameddoo@gmail.com>
2024-05-09 19:19:24 +0300

    finishing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,22 +3,22 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import AboutUs from './About';
 import Contact from './Contacts';
-
+import './App.css';
 
 function NavBar() {
   return (
     <Router>
       <div>
-        <nav className='text-white'>
-          <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 ,textDecorationColor:'text-white'}}>
+        <nav>
+          <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
             <li style={{ marginRight: '10px' }}>
-              <Link to="/" style={{ color: 'white' }}>Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li style={{ marginRight: '10px' }}>
-              <Link to="/about" style={{ color: 'white' }}>About Us</Link>
+              <Link to="/about">About Us</Link>
             </li>
             <li>
-              <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+              <Link to="/contact">Contact</Link>
             </li>
           </ul>
         </nav>
